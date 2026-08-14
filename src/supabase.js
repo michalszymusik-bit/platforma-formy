5bed9ada0b8ae19f1aa38c002d4110a9aa54cf1f
michalszymusik-bit <michalszymusik@gmail.com>
2026-08-14 12:21:47 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://gobkbdgpwozexjyorol.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvYmtiZGdwd3pveGV4anlvcm9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2Njc0NDgsImV4cCI6MjEwMjI0MzQ0OH0.HmdNf25WW8HsjSO1aK2O6s7kqn012PZC3nutRa098Eg'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
